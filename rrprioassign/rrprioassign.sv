7a6788ec64c528ee7850439b34ab75775cebdc96
Marco Zanzottera <marcozanzottera@e.email>
2024-12-06 09:10:21 +0000

    Make component fully asynchronous

--- rrprioassign/rrprioassign.sv
+++ rrprioassign/rrprioassign.sv
@@ -1,13 +1,11 @@
 module rrprioassign #(
     parameter int N = 4  //Fixed number of possible requestors
 ) (
-    input logic clk,
-    input logic en,
     //Vector of requests - 1 at position i if and only if requestor i wants access
-    input logic [N-1:0] r,
+    input  logic [N-1:0] r,
     //Vector of priorities - exactly a 1 in position i, requestor i has the highest priority,
     //then following requestors have decreasing priority (at the end rewind at 0th requestor)
-    input logic [N-1:0] p,
+    input  logic [N-1:0] p,
     //Vector containing at most a 1 in the position of the allowed requestor
     output logic [N-1:0] res
 );

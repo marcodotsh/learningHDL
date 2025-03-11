0166f6752909ab5cfa3e543645688a215d809e97
Marco Zanzottera <marcozanzottera@e.email>
2025-03-11 21:53:59 +0000

    Do not remove Vivado setup file

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -58,7 +58,8 @@ wget ${MIRROR_IP}/${VIVADO_TAR_FILE}.tar.gz -q
 fi
 tar xzf ${VIVADO_TAR_FILE}.tar.gz
 ./${VIVADO_TAR_FILE}/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config ./.devcontainer/install_config.txt
-rm -rf ${VIVADO_TAR_FILE}*
+rm -rf ${VIVADO_TAR_FILE}/*
+rmdir ${VIVADO_TAR_FILE}
 
 echo "source /opt/Xilinx/Vivado/${VIVADO_VERSION}/settings64.sh" >> /root/.profile
 
